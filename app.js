75f1083db7a5e8c2c3e9f2ae488a18ac13e765f0
Aleksey Zhevlakov <aszhev@yandex.ru>
2023-04-23 17:37:37 +0300

    fix: эксперимент 3 для прохождения тестов

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const express = require('express');
-// const rateLimit = require('express-rate-limit');
-// const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 
 const app = express();
-const { PORT = 3005 } = process.env;
+const { PORT = 3000 } = process.env;
 const router = require('./routes');
 
-// const limiter = rateLimit(
-//   {
-//     windowMs: 15 * 60 * 1000, // 15 minutes
-//     max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-//   },
-// );
+const limiter = rateLimit(
+  {
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  },
+);
 
-// app.use(helmet());
-// app.use(limiter);
+app.use(helmet());
+app.use(limiter);
 
 app.use(express.json());
 
